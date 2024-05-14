14987fbdf4c7e2e08b73fd065dee19037fea63d5
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2024-05-14 15:13:41 -0400

    feat: :sparkles: added santa isabel spider

---
+++ src/enum.ts
@@ -0,0 +1,5 @@
+export enum TimeUnit {
+  SEC = 1000,
+  MIN = 60000,
+  HOUR = 3600000,
+}
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,25 @@
+import { TimeUnit } from './enum.js'
 import { Jumbo } from './spiders/Jumbo.js'
+import { Santa } from './spiders/Santa.js'
 
 while (true) {
   console.time('Jumbo')
   const jumboSpider = new Jumbo()
-  const [updated, completed, incompleted] = await jumboSpider.run([])
-  console.log('Updated:', updated.length)
-  console.log('Completed:', completed.length)
-  console.log('Incompleted:', incompleted.length)
+  const [jumboUpdated, jumboCompleted, jumboIncompleted] = await jumboSpider.run([])
+  console.log('Jumbo Updated:', jumboUpdated.length)
+  console.log('Jumbo Completed:', jumboCompleted.length)
+  console.log('Jumbo Incompleted:', jumboIncompleted.length)
   console.timeEnd('Jumbo')
 
-  await new Promise(resolve => setTimeout(resolve, 60000))
+  await new Promise(resolve => setTimeout(resolve, 20 * TimeUnit.SEC))
+
+  console.time('Santa')
+  const santaSpider = new Santa()
+  const [santaUpdated, santaCompleted, santaIncompleted] = await santaSpider.run([])
+  console.log('Santa Updated:', santaUpdated.length)
+  console.log('Santa Completed:', santaCompleted.length)
+  console.log('Santa Incompleted:', santaIncompleted.length)
+  console.timeEnd('Santa')
+
+  await new Promise(resolve => setTimeout(resolve, 2 * TimeUnit.MIN))
 }
