35f33336d9434e6d19c974b073a3b4ece21fb7e6
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2024-05-15 00:39:14 -0400

    feat: :sparkles: added updates many websites

--- src/db/websites.ts
+++ src/db/websites.ts
@@ -1,5 +1,7 @@
-import { Db } from 'mongodb'
-import { Website } from '../types'
+import { Db, ObjectId } from 'mongodb'
+import { Website, WebsiteDB } from '../types'
+import { saveOrUpdateRecord, saveRecord } from './records.js'
+import { Updater } from '../classes/Updater'
 
 export const getAllPaths = async (db: Db): Promise<string[]> => {
   try {
@@ -10,5 +12,53 @@ export const getAllPaths = async (db: Db): Promise<string[]> => {
   } catch (error) {
     console.error('Error al obtener los paths de los websites')
     return []
+  }
+}
+
+export const updateManyWebsites = async (db: Db, updates: Updater[], watcher: number): Promise<void> => {
+  try {
+    const collection = db.collection<Website>('websites')
+    await Promise.all(updates.map(async (update) => {
+      try {
+        const website = await collection.findOne<WebsiteDB>({ path: update.url })
+        if (website === null) return
+
+        await collection.findOneAndUpdate(
+          { _id: website._id },
+          { $set: { price: update.price, best_price: update.bestPrice, average: update.average, last_update: watcher, in_stock: true } }
+        )
+
+        let recordId: ObjectId | undefined
+        if (website.records.length === 0) {
+          recordId = await saveRecord(db, update.bestPrice as number)
+        } else {
+          const lastRecordId = website.records[website.records.length - 1]
+          recordId = await saveOrUpdateRecord(db, lastRecordId, update.bestPrice as number)
+        }
+
+        if (recordId !== undefined) {
+          await collection.findOneAndUpdate(
+            { _id: website._id },
+            { $push: { records: recordId } }
+          )
+        }
+      } catch (error) {
+        console.error('Error al actualizar el website ', update.url, error)
+      }
+    }))
+  } catch (error) {
+    console.error('Error al obtener la colección de los websites')
+  }
+}
+
+export const updateWebsitesWithoutStock = async (db: Db, watcher: number): Promise<void> => {
+  try {
+    const collection = db.collection<Website>('websites')
+    await collection.updateMany(
+      { last_update: { $ne: watcher } },
+      { $set: { price: 0, best_price: 0, in_stock: false } }
+    )
+  } catch (error) {
+    console.error('Error al limpiar los websites sin stock')
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { dbConnect, dbDisconnect } from './db/client.js'
 import { deleteManyDrinks, saveManyDrinks } from './db/drinks.js'
-import { getAllPaths } from './db/websites.js'
+import { getAllPaths, updateManyWebsites, updateWebsitesWithoutStock } from './db/websites.js'
 import { TimeUnit } from './enum.js'
 import { Jumbo } from './spiders/Jumbo.js'
 import { Lider } from './spiders/Lider.js'
@@ -9,39 +9,56 @@ import { Santa } from './spiders/Santa.js'
 while (true) {
   const db = await dbConnect()
   if (db === undefined) process.exit(1)
+  console.log('----------------------------------------------------------')
 
+  // obtener los paths y guardar los drinks
   const paths = await getAllPaths(db)
   await saveManyDrinks(db)
 
-  console.time('Jumbo')
+  // inicializa el watcher y los productos no encontrados
+  const watcher = new Date().getTime()
+
+  //! JUMBO
+  console.time('Jumbo Scraping')
   const jumboSpider = new Jumbo()
   const [jumboUpdated, jumboCompleted, jumboIncompleted] = await jumboSpider.run(paths)
   console.log('Jumbo Updated:', jumboUpdated.length)
+  await updateManyWebsites(db, jumboUpdated, watcher)
   console.log('Jumbo Completed:', jumboCompleted.length)
   console.log('Jumbo Incompleted:', jumboIncompleted.length)
-  console.timeEnd('Jumbo')
+  console.timeEnd('Jumbo Scraping')
+  console.log('----------------------------------------')
 
   await new Promise(resolve => setTimeout(resolve, 30 * TimeUnit.SEC))
 
-  console.time('Santa')
+  //! SANTA ISABEL
+  console.time('Santa Isabel Scraping')
   const santaSpider = new Santa()
   const [santaUpdated, santaCompleted, santaIncompleted] = await santaSpider.run(paths)
   console.log('Santa Updated:', santaUpdated.length)
+  await updateManyWebsites(db, santaUpdated, watcher)
   console.log('Santa Completed:', santaCompleted.length)
   console.log('Santa Incompleted:', santaIncompleted.length)
-  console.timeEnd('Santa')
+  console.timeEnd('Santa Isabel Scraping')
+  console.log('----------------------------------------')
 
   await new Promise(resolve => setTimeout(resolve, 30 * TimeUnit.SEC))
 
-  console.time('Lider')
+  //! LIDER
+  console.time('Lider Scraping')
   const liderSpider = new Lider()
   const [liderUpdated, liderCompleted, liderIncompleted] = await liderSpider.run(paths)
   console.log('Lider Updated:', liderUpdated.length)
+  await updateManyWebsites(db, liderUpdated, watcher)
   console.log('Lider Completed:', liderCompleted.length)
   console.log('Lider Incompleted:', liderIncompleted.length)
-  console.timeEnd('Lider')
+  console.timeEnd('Lider Scraping')
+  console.log('----------------------------------------')
 
+  await updateWebsitesWithoutStock(db, watcher)
   await deleteManyDrinks(db)
   await dbDisconnect()
-  await new Promise(resolve => setTimeout(resolve, 2 * TimeUnit.MIN))
+  console.log('----------------------------------------------------------')
+
+  await new Promise(resolve => setTimeout(resolve, 10 * TimeUnit.MIN))
 }
