20ed791d6db15cc1fcd5a416e2560dc610a846ea
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2024-05-15 13:59:36 -0400

    feat: :sparkles: added run functionality

--- src/index.ts
+++ src/index.ts
@@ -1,65 +1,83 @@
-import { dbConnect, dbDisconnect } from './db/client.js'
-import { deleteManyDrinks, saveManyDrinks } from './db/drinks.js'
-import { getAllPaths, updateManyWebsites, updateWebsitesWithoutStock } from './db/websites.js'
+import { ENVIRONMENT } from './config.js'
 import { TimeUnit } from './enum.js'
 import { Jumbo } from './spiders/Jumbo.js'
 import { Lider } from './spiders/Lider.js'
 import { Santa } from './spiders/Santa.js'
+import { cloudinaryConnect } from './utils/cloudinary.js'
+import { dbConnect, dbDisconnect } from './db/client.js'
+import { getAllPaths, updateManyWebsites, updateWebsitesWithoutStock } from './db/websites.js'
+import { deleteManyDrinks, saveManyDrinks } from './db/drinks.js'
+import { saveManyProducts } from './db/products.js'
+import { Scraper } from './classes/Scraper.js'
+
+console.log('Starting App')
+console.log('Environment:', ENVIRONMENT)
+
+// Connect to Cloudinary
+cloudinaryConnect()
 
 while (true) {
   const db = await dbConnect()
   if (db === undefined) process.exit(1)
-  console.log('----------------------------------------------------------')
 
   // obtener los paths y guardar los drinks
   const paths = await getAllPaths(db)
-  await saveManyDrinks(db)
+  const drinks = await saveManyDrinks(db)
 
   // inicializa el watcher y los productos no encontrados
   const watcher = new Date().getTime()
+  const notFoundProducts: Scraper[] = []
+
   console.log('Watcher:', watcher)
+  console.log('-------------------------------------------------------------------------------------------')
+  await new Promise(resolve => setTimeout(resolve, 30 * TimeUnit.SEC))
 
   //! JUMBO
   console.time('Jumbo Scraping')
   const jumboSpider = new Jumbo()
   const [jumboUpdated, jumboCompleted, jumboIncompleted] = await jumboSpider.run(paths)
-  console.log('Jumbo Updated:', jumboUpdated.length)
   await updateManyWebsites(db, jumboUpdated, watcher)
-  console.log('Jumbo Completed:', jumboCompleted.length)
-  console.log('Jumbo Incompleted:', jumboIncompleted.length)
+  const jumboNotFound = await saveManyProducts(db, jumboCompleted, drinks, jumboSpider.info, watcher)
   console.timeEnd('Jumbo Scraping')
-  console.log('----------------------------------------')
 
+  notFoundProducts.push(...jumboNotFound)
+  notFoundProducts.push(...jumboIncompleted)
+
+  console.log('---------------------------------------------------------')
   await new Promise(resolve => setTimeout(resolve, 30 * TimeUnit.SEC))
 
   //! SANTA ISABEL
   console.time('Santa Isabel Scraping')
   const santaSpider = new Santa()
   const [santaUpdated, santaCompleted, santaIncompleted] = await santaSpider.run(paths)
-  console.log('Santa Updated:', santaUpdated.length)
   await updateManyWebsites(db, santaUpdated, watcher)
-  console.log('Santa Completed:', santaCompleted.length)
-  console.log('Santa Incompleted:', santaIncompleted.length)
+  const santaNotFound = await saveManyProducts(db, santaCompleted, drinks, santaSpider.info, watcher)
   console.timeEnd('Santa Isabel Scraping')
-  console.log('----------------------------------------')
 
+  notFoundProducts.push(...santaNotFound)
+  notFoundProducts.push(...santaIncompleted)
+
+  console.log('---------------------------------------------------------')
   await new Promise(resolve => setTimeout(resolve, 30 * TimeUnit.SEC))
 
   //! LIDER
   console.time('Lider Scraping')
   const liderSpider = new Lider()
   const [liderUpdated, liderCompleted, liderIncompleted] = await liderSpider.run(paths)
-  console.log('Lider Updated:', liderUpdated.length)
   await updateManyWebsites(db, liderUpdated, watcher)
-  console.log('Lider Completed:', liderCompleted.length)
-  console.log('Lider Incompleted:', liderIncompleted.length)
+  const liderNotFound = await saveManyProducts(db, liderCompleted, drinks, liderSpider.info, watcher)
   console.timeEnd('Lider Scraping')
-  console.log('----------------------------------------')
+
+  notFoundProducts.push(...liderNotFound)
+  notFoundProducts.push(...liderIncompleted)
+
+  console.log('---------------------------------------------------------')
+  await new Promise(resolve => setTimeout(resolve, 30 * TimeUnit.SEC))
 
   await updateWebsitesWithoutStock(db, watcher)
   await deleteManyDrinks(db)
   await dbDisconnect()
-  console.log('----------------------------------------------------------')
+  console.log('-------------------------------------------------------------------------------------------')
 
   await new Promise(resolve => setTimeout(resolve, 10 * TimeUnit.MIN))
 }
