71b8c39f18137728aea145f9d15de35ff686f183
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2024-06-19 09:58:26 -0400

    feat: :sparkles: added lider data in updater & scraper

--- src/classes/Updater.ts
+++ src/classes/Updater.ts
@@ -1,4 +1,4 @@
-import type { CencosudProduct } from '../spiders/types'
+import type { CencosudProduct, LiderProduct } from '../spiders/types'
 
 export class Updater {
   productSku: string | undefined
@@ -23,6 +23,17 @@ export class Updater {
       this.bestPrice = data.items[0].sellers[0].commertialOffer.Price
     } catch (error) {
       console.error('Error when obtaining the data to update')
+    }
+  }
+
+  public setLiderData (data: LiderProduct, pageUrl: string): void {
+    try {
+      this.productSku = data.sku
+      this.url = `${pageUrl}/supermercado/product/sku/${data.sku}`
+      this.price = data.price.BasePriceReference
+      this.bestPrice = data.price.BasePriceSales
+    } catch (error) {
+      console.error('Error al obtener los datos a actualizar', error)
     }
   }
 }
