f291def2bcd6b5b580addb2b1765e3ab53e366f2
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2024-06-19 09:59:00 -0400

    refactor: :art: added sleep times

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,5 +1,18 @@
+import { sleep } from 'bun'
+import { TimeUnit } from './enums'
 
 export const isSaturday = (): boolean => {
   const today = new Date()
   return today.getDay() === 6
+}
+
+export const sleepBetweenSpiders = async (): Promise<void> => {
+  await sleep(5 * TimeUnit.SEC)
+  console.log('---------------------------------------------------------')
+  await sleep(5 * TimeUnit.SEC)
+}
+
+export const sleepStartEndSpiders = async (): Promise<void> => {
+  await sleep(5 * TimeUnit.SEC)
+  console.log('---------------------------------------------------------------------------------------------')
 }
