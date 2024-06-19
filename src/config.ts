d450c9f6f826a3b1134bf95aba7d2be4908a068d
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2024-06-19 03:28:59 -0400

    feat: :sparkles: added config env variables

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+export const DRINKS_API = Bun.env.DRINKS_API
+export const PROXIES_API = Bun.env.PROXIES_API
+export const DB_URI = Bun.env.DB_URI
+export const RESEND_KEY = Bun.env.RESEND_API_KEY
+export const CLOUDINARY_NAME = Bun.env.CLOUDINARY_NAME
+export const CLOUDINARY_API_KEY = Bun.env.CLOUDINARY_API_KEY
+export const CLOUDINARY_API_SECRET = Bun.env.CLOUDINARY_SECRET_KEY
+export const ENVIRONMENT = Bun.env.NODE_ENV
