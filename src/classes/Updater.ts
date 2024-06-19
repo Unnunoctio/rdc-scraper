796e39f4acf958b21629adaa2dee1e1f19c3a093
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2024-06-19 06:22:09 -0400

    feat: :sparkles: added cencosud data in Updater

--- src/classes/Updater.ts
+++ src/classes/Updater.ts
@@ -1,3 +1,4 @@
+import type { CencosudProduct } from '../spiders/types'
 
 export class Updater {
   productSku: string | undefined
@@ -12,5 +13,16 @@ export class Updater {
 
   public isComplete (): boolean {
     return this.productSku !== undefined && this.url !== undefined && this.price !== undefined && this.price > 0 && this.bestPrice !== undefined && this.bestPrice > 0 && this.average !== null
+  }
+
+  public setCencosudData (data: CencosudProduct, pageUrl: string): void {
+    try {
+      this.productSku = data.productId
+      this.url = `${pageUrl}/${data.linkText}/p`
+      this.price = data.items[0].sellers[0].commertialOffer.PriceWithoutDiscount
+      this.bestPrice = data.items[0].sellers[0].commertialOffer.Price
+    } catch (error) {
+      console.error('Error when obtaining the data to update')
+    }
   }
 }
