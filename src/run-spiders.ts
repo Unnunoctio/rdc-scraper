77db1ad956ff69a7025c17f5d6dfc432287fd1af
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2024-06-20 08:35:27 -0400

    chore: :bulb: uncomment code

--- src/run-spiders.ts
+++ src/run-spiders.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import type { Db } from 'mongodb'
 import type { DrinkDB } from './types'
 import type { Scraper } from './classes'
@@ -14,13 +13,12 @@ import { Jumbo, Lider, Santa } from './spiders'
 const runSpider = async (db: Db, spider: Spider, name: SpiderName, paths: string[], drinks: DrinkDB[], watcher: number): Promise<Scraper[]> => {
   console.time(`${name} Scraping`)
   const [updated, completed, incompleted] = await spider.run(paths)
-  // await updateManyWebsites(db, updated, watcher)
-  // const notFound = await saveManyProducts(db, completed, drinks, spider.info, watcher)
+  await updateManyWebsites(db, updated, watcher)
+  const notFound = await saveManyProducts(db, completed, drinks, spider.info, watcher)
   console.timeEnd(`${name} Scraping`)
 
   console.log(`Updated: ${updated.length}  -  Completed: ${completed.length}  -  Incompleted: ${incompleted.length}`)
-  // return [...notFound, ...incompleted]
-  return []
+  return [...notFound, ...incompleted]
 }
 
 export const runSpiders = async (): Promise<Scraper[]> => {
@@ -40,23 +38,23 @@ export const runSpiders = async (): Promise<Scraper[]> => {
   const jumboNotFound = await runSpider(db, new Jumbo(), SpiderName.JUMBO, paths, drinks, watcher)
   notFound.push(...jumboNotFound)
 
-  // await sleepBetweenSpiders()
+  await sleepBetweenSpiders()
 
-  // // TODO: SANTA
-  // const santaNotFound = await runSpider(db, new Santa(), SpiderName.SANTA, paths, drinks, watcher)
-  // notFound.push(...santaNotFound)
+  // TODO: SANTA
+  const santaNotFound = await runSpider(db, new Santa(), SpiderName.SANTA, paths, drinks, watcher)
+  notFound.push(...santaNotFound)
 
-  // await sleepBetweenSpiders()
+  await sleepBetweenSpiders()
 
-  // // TODO: LIDER
-  // const liderNotFound = await runSpider(db, new Lider(), SpiderName.LIDER, paths, drinks, watcher)
-  // notFound.push(...liderNotFound)
+  // TODO: LIDER
+  const liderNotFound = await runSpider(db, new Lider(), SpiderName.LIDER, paths, drinks, watcher)
+  notFound.push(...liderNotFound)
 
   await sleepStartEndSpiders()
 
-  // // TODO: UPDATE DB && DISCONNECT
-  // await updateWebsitesWithoutStock(db, watcher)
-  // await deleteManyDrinks(db)
+  // TODO: UPDATE DB && DISCONNECT
+  await updateWebsitesWithoutStock(db, watcher)
+  await deleteManyDrinks(db)
   await dbDisconnect()
 
   return notFound
