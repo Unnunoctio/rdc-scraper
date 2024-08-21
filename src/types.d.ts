cc995267966d9e8aea31bad82d4715385b67b9e7
rafael.carvacho.p <rafael.carvacho.p@gmail.com>
2024-08-21 18:04:18 +0000

    feat: :label: added drinks types

---
+++ src/types.d.ts
@@ -0,0 +1,60 @@
+import type { ObjectId } from 'mongoose'
+
+// BD
+export interface DrinkDB extends Drink {
+  _id: ObjectId
+  createdAt?: Date
+  updatedAt?: Date
+}
+
+// INTERFACE
+export interface Drink {
+  drinkId: string
+  name: string
+  brand: string
+  abv: number
+  volume: number
+  packaging: string
+  category: string
+  subCategory: string
+  origin: string
+  variety?: string
+  ibu?: number
+  servingTemp?: string
+  strain?: string
+  vineyard?: string
+}
+
+// API
+export interface DrinksApiResponse {
+  status: boolean
+  data: DrinkApi[]
+  error: any
+  meta: {
+    timestamp: string
+    version: string
+    pagination: {
+      page: number
+      limit: number
+      totalPages: number
+      totalItems: number
+    }
+  }
+}
+
+export interface DrinkApi {
+  _id?: string
+  name: string
+  brand: string
+  abv: number
+  volume: number
+  packaging: string
+  category: string
+  subCategory: string
+  origin: string
+  variety?: string
+  ibu?: number
+  servingTemp?: string
+  strain?: string
+  vineyard?: string
+}
