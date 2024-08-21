b8f9fba492a3fd48ccb90557caa38eaa89667e9e
rafael.carvacho.p <rafael.carvacho.p@gmail.com>
2024-08-21 18:06:30 +0000

    feat: :sparkles: add drink model

---
+++ src/db/drink-model.ts
@@ -0,0 +1,27 @@
+import { Schema, model } from 'mongoose'
+import type { DrinkDB } from '../types'
+
+const DrinkSchema = new Schema({
+  drinkId: { type: String, required: true, index: true },
+  name: { type: String, required: true },
+  brand: { type: String, required: true, index: true },
+  abv: { type: Number, required: true, min: 0, max: 100, index: true },
+  volume: { type: Number, required: true, min: 0, index: true }, // in cc
+  packaging: { type: String, required: true, index: true },
+  category: { type: String, required: true },
+  subCategory: { type: String, required: true },
+  origin: { type: String, required: true },
+  // ? CERVEZAS
+  variety: { type: String },
+  ibu: { type: Number, min: 0, max: 100 },
+  servingTemp: { type: String },
+  // ? VINOS
+  strain: { type: String },
+  vineyard: { type: String }
+}, {
+  timestamps: true,
+  versionKey: false
+})
+
+const DrinkModel = model<DrinkDB>('Drink', DrinkSchema, 'drinks')
+export default DrinkModel
