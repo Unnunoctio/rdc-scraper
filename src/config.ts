8a8e07d10b1026dade0cee8feaf4108440436392
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2024-08-22 16:43:23 -0400

    refactor: :art: add cloudinary forder into env

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,7 @@ export const DRINKS_API = Bun.env.DRINKS_API
 export const DB_URI = Bun.env.DB_URI
 export const RESEND_KEY = Bun.env.RESEND_API_KEY
 export const CLOUDINARY_NAME = Bun.env.CLOUDINARY_NAME
+export const CLOUDINARY_FOLDER = Bun.env.CLOUDINARY_FOLDER
 export const CLOUDINARY_API_KEY = Bun.env.CLOUDINARY_API_KEY
 export const CLOUDINARY_API_SECRET = Bun.env.CLOUDINARY_SECRET_KEY
 export const ENVIRONMENT = Bun.env.NODE_ENV
