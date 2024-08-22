a96678d1b0012145901d55e834f5af63b9b7111c
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2024-08-22 16:44:56 -0400

    feat: :sparkles: add find drink

--- src/service/drink-service.ts
+++ src/service/drink-service.ts
@@ -1,21 +1,32 @@
 import DrinkModel from '../db/drink-model'
+import type { DrinkDB } from '../types'
+import type { Scraper } from '../classes'
 import { getDrinksApi } from '../utils/drinks-api'
 
 const saveManyDrinksByApi = async (): Promise<void> => {
   const drinksApi = await getDrinksApi()
-  const drinksFormatted = drinksApi.map(d => {
-    const drinkId = d._id
-    delete d._id
-    return {
-      drinkId,
-      ...d
-    }
-  })
 
-  const updateOperations = drinksFormatted.map(drink => ({
+  const updateOperations = drinksApi.map(drink => ({
     updateOne: {
-      filter: { drinkId: drink.drinkId },
-      update: { $set: drink },
+      filter: { drinkId: drink._id },
+      update: {
+        $set: {
+          drinkId: drink._id,
+          name: drink.name,
+          brand: drink.brand,
+          abv: drink.abv,
+          volume: drink.volume,
+          packaging: drink.packaging,
+          category: drink.category,
+          subCategory: drink.subCategory,
+          origin: drink.origin,
+          variety: drink.variety,
+          ibu: drink.ibu,
+          servingTemp: drink.servingTemp,
+          strain: drink.strain,
+          vineyard: drink.vineyard
+        }
+      },
       upsert: true
     }
   }))
@@ -23,6 +34,32 @@ const saveManyDrinksByApi = async (): Promise<void> => {
   await DrinkModel.bulkWrite(updateOperations)
 }
 
+const findDrink = async (product: Scraper): Promise<DrinkDB | undefined> => {
+  const drinks = await DrinkModel.find({ brand: product.brand, volume: product.content, packaging: product.package, abv: product.alcoholicGrade }, { createdAt: 0, updatedAt: 0 }).lean().exec()
+  if (drinks.length === 0) return undefined
+
+  let selected: DrinkDB | undefined
+  let matching = -1
+
+  const titleSplit = product.title?.toLocaleLowerCase().split(' ').filter(w => w !== '') as string[]
+
+  for (const d of drinks) {
+    const nameSplit = d.name?.toLowerCase().split(' ').filter(w => w !== '')
+    const isMatching = nameSplit.every(w => titleSplit.includes(w))
+
+    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+    if (isMatching && (nameSplit.length > matching)) {
+      matching = nameSplit.length
+      selected = d
+    }
+  }
+
+  return selected
+}
+
+// TODO: DELETE MANY DRINKS
+
 export const drinkService = {
-  saveManyDrinksByApi
+  saveManyDrinksByApi,
+  findDrink
 }
