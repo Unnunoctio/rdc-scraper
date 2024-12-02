2d867e450c940fe565486cd1871c785a7fa31b6b
Unnunoctio <unnunoctio.dev@gmail.com>
2024-12-02 17:29:24 -0300

    refactor: :art: change resend from email

--- src/services/email/resend.ts
+++ src/services/email/resend.ts
@@ -1,21 +1,21 @@
-import { Resend } from 'resend'
 import { RESEND_KEY } from '@/config'
 import type { Scraper } from '@/scraping/classes'
 import { createExcel } from '@/utils/excel'
+import { Resend } from 'resend'
 
 export const sendEmail = async (products: Scraper[]): Promise<void> => {
   const beersFile = await createExcel(products.filter((p) => p.category === 'Cervezas'), 'not-found-beers')
   const winesFile = await createExcel(products.filter((p) => p.category === 'Vinos'), 'not-found-wines')
   const spiritsFile = await createExcel(products.filter((p) => p.category === 'Destilados'), 'not-found-spirits')
-  
+
   const resend = new Resend(RESEND_KEY)
 
   try {
     await resend.emails.send({
-      from: 'scraping@resend.dev',
+      from: 'onboarding@resend.dev',
       to: 'rincondelcurao@gmail.com',
       subject: 'Scraping not found results',
-      html: `<h1>Products not found</h1>`,
+      html: '<h1>Products not found</h1>',
       attachments: [
         beersFile,
         winesFile,
