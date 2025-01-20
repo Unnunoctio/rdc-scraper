d0bc0b33db2ae29c2ab73047f402a0d0abe37280
Unnunoctio <unnunoctio.dev@gmail.com>
2025-01-20 20:43:11 -0300

    feat: :sparkles: add send email to index

--- src/index.py
+++ src/index.py
@@ -1,7 +1,9 @@
 import asyncio
 import time
 import traceback
+from datetime import datetime
 
+from config import RESEND_SEND
 from services.mongodb.drink_table_service import DrinkTableService
 from services.mongodb.mongodb import MongoDB
 from services.mongodb.product_table_service import ProductTableService
@@ -11,6 +13,7 @@ from spiders.santa_spider import SantaSpider
 from spiders.spider_interface import SpiderInterface
 from spiders.spider_types import SpiderName
 from utils.generate import generate_watcher
+from utils.resend import send_email
 
 
 async def run_spider(spider: SpiderInterface, name: SpiderName, paths: list[str], drinks: list[dict], watcher: str, product_service: ProductTableService, website_service: WebsiteTableService, drink_service: DrinkTableService) -> list:
@@ -35,23 +38,27 @@ async def main(mongodb_connection: MongoDB) -> None:
     paths = website_service.get_all_paths()
     drinks = drink_service.get_drinks()
     watcher = generate_watcher()
-    not_found_drinks = []
+    not_found_products = []
 
     print(f"Watcher: {watcher}\n")
     # TODO: JUMBO
     jumbo_not_found = await run_spider(spider=JumboSpider(), name=SpiderName.JUMBO, paths=paths, drinks=drinks, watcher=watcher, product_service=product_service, website_service=website_service, drink_service=drink_service)
-    not_found_drinks.extend(jumbo_not_found)
+    not_found_products.extend(jumbo_not_found)
     time.sleep(5)
 
     # TODO: SANTA
     santa_not_found = await run_spider(spider=SantaSpider(), name=SpiderName.SANTA, paths=paths, drinks=drinks, watcher=watcher, product_service=product_service, website_service=website_service, drink_service=drink_service)
-    not_found_drinks.extend(santa_not_found)
+    not_found_products.extend(santa_not_found)
     time.sleep(5)
 
     # TODO: LIDER
 
     # TODO: UPADTE DB
     website_service.update_websites_without_stock(watcher=watcher)
+
+    # TODO: EMAIL 
+    if RESEND_SEND == "SEND" and datetime.now().weekday() == 5:
+        send_email(products=not_found_products)
 
 
 try:
